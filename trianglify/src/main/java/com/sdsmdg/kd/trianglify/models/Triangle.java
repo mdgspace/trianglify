d55a66a20e9ed480fb7be939e2988f7267ed3a7a
Karan Desai <karandesai_96@live.com>
2017-01-18 15:29:08 +0530

    Add 'contains()' to check if a point is in interior of triangle.

--- trianglify/src/main/java/com/sdsmdg/kd/trianglify/models/Triangle.java
+++ trianglify/src/main/java/com/sdsmdg/kd/trianglify/models/Triangle.java
@@ -13,4 +13,22 @@ public class Triangle {
         this.b = b;
         this.c = c;
     }
+
+    private float sign (Point p1, Point p2, Point p3) {
+        return (p1.x - p3.x) * (p2.y - p3.y) - (p1.y - p3.y) * (p2.x - p3.x);
+    }
+
+    public boolean contains (Point p) {
+        boolean pab, pbc, pca;
+
+        pab = sign(p, a, b) < 0f;
+        pbc = sign(p, b, c) < 0f;
+
+        if (pab == pbc)
+            return false;
+
+        pca = sign(p, c, a) < 0f;
+
+        return (pab == pca);
+    }
 }
