1d4d19bf487872424e731e46c0ffa838e2eb47fe
Karan Desai <karandesai_96@live.com>
2017-01-18 19:42:23 +0530

    Rename method sign-> isClockwise, simplify contains method body.

--- trianglify/src/main/java/com/sdsmdg/kd/trianglify/models/Triangle.java
+++ trianglify/src/main/java/com/sdsmdg/kd/trianglify/models/Triangle.java
@@ -14,21 +14,11 @@ public class Triangle {
         this.c = c;
     }
 
-    private float sign (Point p1, Point p2, Point p3) {
-        return (p1.x - p3.x) * (p2.y - p3.y) - (p1.y - p3.y) * (p2.x - p3.x);
+    private boolean isClockwise (Point p1, Point p2, Point p3) {
+        return (p1.x - p3.x) * (p2.y - p3.y) - (p1.y - p3.y) * (p2.x - p3.x) < 0f;
     }
 
     public boolean contains (Point p) {
-        boolean pab, pbc, pca;
-
-        pab = sign(p, a, b) < 0f;
-        pbc = sign(p, b, c) < 0f;
-
-        if (pab == pbc)
-            return false;
-
-        pca = sign(p, c, a) < 0f;
-
-        return (pab == pca);
+        return isClockwise(p, a, b) == isClockwise(p, b, c) == isClockwise(p, c, a);
     }
 }
