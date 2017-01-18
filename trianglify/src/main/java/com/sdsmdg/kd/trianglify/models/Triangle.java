3f1031fbd94bb976eb7b6345d03b360f8577d037
Karan Desai <karandesai_96@live.com>
2017-01-18 15:29:08 +0530

    Declare Triangle class with a simple constructor.

---
+++ trianglify/src/main/java/com/sdsmdg/kd/trianglify/models/Triangle.java
@@ -0,0 +1,16 @@
+package com.sdsmdg.kd.trianglify.models;
+
+
+import android.graphics.Point;
+
+public class Triangle {
+    public Point a;
+    public Point b;
+    public Point c;
+
+    public Triangle (Point a, Point b, Point c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+}
