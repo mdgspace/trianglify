c0aad07dd355af0bb54501b6d675bfee5923a96c
Karan Desai <karandesai_96@live.com>
2017-01-19 09:45:44 +0530

    Try out grid generator in trianglify view.

--- trianglify/src/main/java/com/sdsmdg/kd/trianglify/TrianglifyView.java
+++ trianglify/src/main/java/com/sdsmdg/kd/trianglify/TrianglifyView.java
@@ -4,25 +4,35 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.sdsmdg.kd.trianglify.generators.GridGenerator;
+
+import java.util.Vector;
 
 
 public class TrianglifyView extends View {
 
     public Paint mPaint = new Paint();
+    public GridGenerator gridGenerator = new GridGenerator();
 
     public TrianglifyView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         // temporary background to check whether the view is being rendered
         setBackgroundColor(Color.parseColor("#FF000000"));
-        mPaint.setColor(Color.parseColor("#FFFFFFFF"));
+        mPaint.setColor(Color.parseColor("#66FFFFFF"));
     }
 
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawCircle(getMeasuredWidth() / 2, getMeasuredHeight() / 2, 100, mPaint);
+        Vector<Point> grid = gridGenerator.generateGrid(getMeasuredWidth(), getMeasuredHeight(), 60, 30);
+
+        for (Point p : grid) {
+            canvas.drawCircle(p.x, p.y, 10, mPaint);
+        }
     }
 }
