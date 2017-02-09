009e4167d615abbd0856fea20265cb2b92aa4c1a
dakshitagrawal97 <neelagrawal08@gmail.com>
2017-02-09 21:21:26 +0530

    Merged two methods into one grid generator method (#1)

--- trianglify/src/main/java/com/sdsmdg/kd/trianglify/generators/GridGenerator.java
+++ trianglify/src/main/java/com/sdsmdg/kd/trianglify/generators/GridGenerator.java
@@ -13,50 +13,49 @@ public class GridGenerator {
     private int pointsPerCircle = 8;
 
     public Vector<Point> grid = new Vector<>();
-    public enum GridType { CIRCULAR, RECTANGULAR };
+
+    public enum GridType {CIRCULAR, RECTANGULAR}
+
+    ;
     public GridType currentGridtype = GridType.RECTANGULAR;
 
-    public GridGenerator() {}
+    public GridGenerator() {
+    }
 
     public GridGenerator(int bleedX, int bleedY) {
         this.bleedX = bleedX;
         this.bleedY = bleedY;
     }
 
-    public void generateRectangularGrid(int width, int height, int cellSize, int variance) {
+    public void generateGrid(int width, int height, int cellSize, int variance) {
         this.grid.clear();
-        this.currentGridtype = GridType.RECTANGULAR;
+        
+        if (this.currentGridtype == GridType.RECTANGULAR) {
+            int x, y;
+            for (int j = -bleedY; j < height + bleedY; j += cellSize) {
+                for (int i = -bleedX; i < width + bleedX; i += cellSize) {
+                    x = i + random.nextInt(variance);
+                    y = j + random.nextInt(variance);
+                    this.grid.add(new Point(x, y));
+                }
+            }
+        } else if (this.currentGridtype == GridType.CIRCULAR) {
+            Point center = new Point(width / 2, height / 2);
+            int maxRadius = Math.max(width + bleedX, height + bleedY);
+            this.grid.add(center);
 
-        int x, y;
-        for (int j = -bleedY; j < height + bleedY; j += cellSize) {
-            for (int i = -bleedX; i < width + bleedX; i += cellSize) {
-                x = i + random.nextInt(variance);
-                y = j + random.nextInt(variance);
-                this.grid.add(new Point(x, y));
+            double slice, angle;
+            int x, y;
+
+            for (int radius = cellSize; radius < maxRadius; radius += cellSize) {
+                slice = 2 * Math.PI / pointsPerCircle;
+                for (int i = 0; i < pointsPerCircle; i++) {
+                    angle = slice * i;
+                    x = (int) (center.x + radius * Math.cos(angle)) + random.nextInt(variance);
+                    y = (int) (center.y + radius * Math.sin(angle)) + random.nextInt(variance);
+                    this.grid.add(new Point(x, y));
+                }
             }
         }
     }
-
-    public void generateCircularGrid(int width, int height, int cellSize, int variance) {
-        this.grid.clear();
-        this.currentGridtype = GridType.CIRCULAR;
-
-        Point center = new Point(width / 2, height / 2);
-        int maxRadius = Math.max(width + bleedX, height + bleedY);
-        this.grid.add(center);
-
-        double slice, angle;
-        int x, y;
-
-        for (int radius = cellSize; radius < maxRadius; radius += cellSize) {
-            slice = 2 * Math.PI / pointsPerCircle;
-            for (int i = 0; i < pointsPerCircle; i++) {
-                angle = slice * i;
-                x = (int) (center.x + radius * Math.cos(angle)) + random.nextInt(variance);
-                y = (int) (center.y + radius * Math.sin(angle)) + random.nextInt(variance);
-                this.grid.add(new Point(x, y));
-            }
-        }
-    }
-
 }
