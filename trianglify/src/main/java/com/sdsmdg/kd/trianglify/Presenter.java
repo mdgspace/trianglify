2c6580ab648b1db85f05c082c454686cdb3ddc21
suyashmahar <suyash12mah.uec2016@iitr.ac.in>
2017-03-18 02:11:48 +0000

    Add basic layout for Presenter.java

---
+++ trianglify/src/main/java/com/sdsmdg/kd/trianglify/Presenter.java
@@ -0,0 +1,48 @@
+package com.sdsmdg.kd.trianglify;
+
+
+import android.graphics.Point;
+import com.sdsmdg.kd.trianglify.models.Grid;
+import com.sdsmdg.kd.trianglify.models.Triangulation;
+import java.util.Vector;
+
+
+/**
+ * Created by suyash on 18/3/17.
+ */
+
+public class Presenter {
+    TrianglifyViewInterface view;
+
+    public Presenter(TrianglifyViewInterface view) {
+        this.view = view;
+    }
+
+    private Grid generateGrid() {
+        //TODO generates Grid by querying presenter.getTypeGrid()
+        return null;
+    }
+
+    private Triangulation generateTriangulation(Grid inputGrid) {
+        //TODO generate Triangulation from inputGrid based on presenter.getTriangulation()
+        return null;
+    }
+
+    private Triangulation generateColoredSoup() {
+        //TODO generates colored soup using presenter.getPallete()
+        return null;
+    }
+
+    public void generateSoup() {
+        //TODO Call generateGrid(), generateTriangulation(), generateColoredSoup()
+    }
+
+    public Triangulation getSoup() {
+        //TODO Implement returns generated soup
+        return null;
+    }
+
+    public void clearSoup() {
+        //TODO sets local soup variable to null
+    }
+}
---
+++ trianglify/src/main/java/com/sdsmdg/kd/trianglify/TrianglifyViewInterface.java
@@ -0,0 +1,8 @@
+package com.sdsmdg.kd.trianglify;
+
+/**
+ * Created by suyash on 18/3/17.
+ */
+
+public class TrianglifyViewInterface {
+}
