fa099d1c90aecc8f9c59bb325c246095dede84bd
suyashmahar <suyash12mah.uec2016@iitr.ac.in>
2017-03-25 23:03:11 +0000

    Add sub, add and midpoint methods in Points.java

--- trianglify/src/main/java/com/sdsmdg/kd/trianglify/utilities/Point.java
+++ trianglify/src/main/java/com/sdsmdg/kd/trianglify/utilities/Point.java
@@ -9,7 +9,7 @@ package com.sdsmdg.kd.trianglify.utilities;
  * @since 25/3/17.
  */
 
-public class Point {
+public class Point{
     public int x;
     public int y;
 
@@ -20,5 +20,23 @@ public class Point {
     public Point(int x, int y){
         this.x = x;
         this.y = y;
+    }
+
+    public static Point subtract(Point a, Point b){
+        return new Point(a.x - a.y, b.x - b.y);
+    }
+
+    public static Point add(Point a, Point b){
+        return new Point(a.x + a.y, b.x + b.y);
+    }
+
+    /**
+     * Calculates mid point of two given points using integer arithmetic
+     * @param a First Point
+     * @param b Second Point
+     * @return Mid Point
+     */
+    public static Point midPoint(Point a, Point b){
+        return new Point((a.x + a.y) / 2, (b.x + b.y) / 2);
     }
 }
